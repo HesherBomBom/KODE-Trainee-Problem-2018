e3c6bc68513bc802983590082182fa7d4114a45e
pzykov <zpavel411@gmail.com>
2022-11-08 16:16:40 +0300

    Initial Commit

---
+++ KODE Trainee Problem 2018/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KODE Trainee Problem 2018
+//
+//  Created by Pavel Zykov on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
